3b85a27777a95648b338684bdf8939c8db3c1d93
Sergey Okatov <sokatov@gmail.com>
2024-05-15 09:06:53 +0500

    Preparing for lesson

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.9.23"
 }
 
 group = "ru.otus.otuskotlin.dsl"
@@ -10,8 +10,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(kotlin("test-junit5"))
 }
 
 tasks.test {
--- src/test/kotlin/DslTDDTest.kt
+++ src/test/kotlin/DslTDDTest.kt
@@ -1,2 +1,11 @@
+package ru.otus.otuskotlin.dsl
+
+import kotlin.test.Test
+
+
 class DslTDDTest {
+    @Test
+    fun dslTest() {
+
+    }
 }
